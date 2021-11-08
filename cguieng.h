c8bdd505c857b20b3e1877bd2db942dfbb7e2666
Mazzei64 <mazzei.ws@gmail.com>
2021-11-08 19:45:37 -0300

    style: reorganizes cguieng.h interface and adds new macro definition for byte, and new byte array variable displayBoundaries.

--- cguieng.h
+++ cguieng.h
@@ -5,6 +5,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define byte unsigned char
+#define clrscr() printf("\e[1;1H\e[2J")
+#define MAX_OBJLIST_SIZE 256
+#define WIDTH 238
+#define HIGHT 60
+#define SCREEN_SIZE 238 * 60
+#define REFRESH_RATE defaut_refresh
+#define SET_REFRESH_RATE(a)     if(a<0) {   \
+                            fprintf(stderr,"\nWARNING: Invalid refresh rate value. Can't be equal or less than 0.\n");   \
+                            }                           \
+                            defaut_refresh = a;
+#define CREATE_DISPLAY_BUFFER displayBuffer	        \
+			 = (char*)malloc(sizeof(char) * WIDTH * HIGHT);
+
 typedef struct SqrObject {
     int id;
     char* canva;
@@ -14,26 +28,16 @@ typedef struct SqrObject {
     int hight;
 } Object;
 
+extern byte displayBoundaries[];
+extern char* displayBuffer;
+
 extern int DestroyObject(Object** object);
 extern Object* NewObject(int width, int hight);
 extern void Refresh();
 extern void SetObject(Object* object);
 extern void UpdateDisplay();
+#define byte unsigned char
 
-#define clrscr() printf("\e[1;1H\e[2J")
-#define MAX_OBJLIST_SIZE 256
-#define WIDTH 238
-#define HIGHT 60
-#define SCREEN_SIZE 238 * 60
 static int defaut_refresh = 15;
-#define REFRESH_RATE defaut_refresh
-#define SET_REFRESH_RATE(a)     if(a<0) {   \
-                            fprintf(stderr,"\nWARNING: Invalid refresh rate value. Can't be equal or less than 0.\n");   \
-                            }                           \
-                            defaut_refresh = a;
-
-extern char* displayBuffer;
-#define CREATE_DISPLAY_BUFFER displayBuffer	        \
-			 = (char*)malloc(sizeof(char) * WIDTH * HIGHT);
 
 #endif
